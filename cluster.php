c48cf8657c46c5320104fe35fa5e9959e3cdf316
Richard Neal <rnealx@gmail.com>
2013-01-06 07:18:23 -0500

    9

--- cluster.php
+++ cluster.php
@@ -62,7 +62,7 @@ function downloadString( $str, $name = "defaultname", $ext = null, $utf8 = true 
 require_once("merge.php");
 
 //$file = "files/merge.tsv";
-$file = "mergediv.tsv";
+$file = getcwd() . "/mergediv.tsv";
 $method = $_POST['method'];
 $metric = $_POST['metric'];
 $output = $_POST['output'];
