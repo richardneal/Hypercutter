9c5c917e6d47f363be32abd4a86a1fb95a4ad694
Scott Kleinman <scott.kleinman@csun.edu>
2013-01-06 12:45:43 -0800

    Fixed a grammatical error in a tooltip message :-)

--- index.php
+++ index.php
@@ -461,7 +461,7 @@ foreach($files as $file) {
 <td width="50%">&nbsp;&nbsp;&nbsp;<input name="hyphens" type="checkbox" value="<?php echo isset($_SESSION['hyphens']) ? $_SESSION['hyphens'] = 'yes' : $_SESSION['hyphens'] = 'no'; ?>"/> <label>Keep Hyphens</label></td></tr>
 <tr><td width="50%"><input name="numbers" type="checkbox" value="<?php echo isset($_SESSION['numbers']) ? $_SESSION['numbers'] = 'yes' : $_SESSION['numbers'] = 'no'; ?>"/> <label for="numbers">Keep Numbers</label></td>
 <td width="50%"><input name="preserve_case" type="checkbox" value="<?php echo isset($_SESSION['preserve_case']) ? $_SESSION['preserve_case'] = 'yes' : $_SESSION['preserve_case'] = 'no'; ?>"/> <label for="preserve_case">Preserve Case</label></td></tr>
-<tr><td width="50%"><a id="stopwords" href="#">Remove Stopwords</a> <img valign="bottom" src="question_mark.png" alt="Question Mark" title="Click the link to upload stopword list (a text file with each stopword separated by a space or a comma)." /></td>
+<tr><td width="50%"><a id="stopwords" href="#">Remove Stopwords</a> <img valign="bottom" src="question_mark.png" alt="Question Mark" title="Click the link to upload a stopword list (a text file with each stopword separated by a space or a comma)." /></td>
 <td width="50%"><a id="advanced" href="#">Advanced Options</a> <img valign="bottom" src="question_mark.png" alt="Question Mark" title="Advanced options allow you to upload a list of token-lemma equivalents, a list of word or character equivalents to consolidate, rules for handling special characters, or custom regular expressions." /></td></tr>
 </table>
 <input type="hidden" id="stopwordlist" name="stopwordlist" value="" />
